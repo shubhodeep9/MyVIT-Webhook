6347c83486431887d949651665687f0d4daf64e9
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-08-24 02:54:06 +0530

    git pull tak hogya

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from sanic import Sanic
 from sanic.response import json
+import json as js
+import os
 
 app = Sanic()
 
-@app.route("/")
+@app.route("/payload", methods=["POST", ])
 async def test(request):
-    return json({"hello": "world"})
+	ref = js.loads(request.form['payload'][0])['ref']
+	if (ref == "refs/heads/prod"):
+		os.chdir("/home/shubhodeep9/go/src/go-MyVIT")
+		import subprocess
+		subprocess.call(["git","checkout","prod"])
+		subprocess.call(["git","pull"])
+	return json({"hello": ref})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
